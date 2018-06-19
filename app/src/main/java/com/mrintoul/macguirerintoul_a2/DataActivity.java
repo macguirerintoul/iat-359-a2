0ea8968fcf08646737d9570707b13546fb1b17f4
Macguire Rintoul <mrintoul@users.noreply.github.com>
2018-06-19 14:19:13 -0700

    app beeps when light sensor is covered

--- app/src/main/java/com/mrintoul/macguirerintoul_a2/DataActivity.java
+++ app/src/main/java/com/mrintoul/macguirerintoul_a2/DataActivity.java
@@ -2,6 +2,9 @@ package com.mrintoul.macguirerintoul_a2;
 
 import android.content.Intent;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,15 +12,50 @@ import android.widget.TextView;
 
 import java.util.List;
 
-public class DataActivity extends AppCompatActivity {
-    TextView sensorName;
+public class DataActivity extends AppCompatActivity implements SensorEventListener {
+    TextView sensorName, valuesTextView;
+    SensorManager sensorManager;
+    Sensor thisSensor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+        sensorManager = MainActivity.sensorManager;
         sensorName = findViewById(R.id.sensorName);
+        valuesTextView = findViewById(R.id.valuesTextView);
+
         Intent i = getIntent();
-        Sensor thisSensor = MainActivity.mySensorManager.getDefaultSensor(i.getIntExtra("SENSOR_TYPE", Sensor.TYPE_ALL));
+        thisSensor = sensorManager.getDefaultSensor(i.getIntExtra("SENSOR_TYPE", Sensor.TYPE_ALL));
         sensorName.setText(thisSensor.getName());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, thisSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        sensorManager.unregisterListener(this);
+        super.onPause();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float[] values = event.values;
+        String valueText = values[0] + "\n";
+        if (values.length > 1) {
+            valueText += values[1] + "\n";
+        }
+        if (values.length > 2) {
+            valueText += values[2] + "\n";
+        }
+        valuesTextView.setText(valueText);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
--- app/src/main/java/com/mrintoul/macguirerintoul_a2/MainActivity.java
+++ app/src/main/java/com/mrintoul/macguirerintoul_a2/MainActivity.java
@@ -1,28 +1,34 @@
 package com.mrintoul.macguirerintoul_a2;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private RecyclerView sensorRecyclerView;
     private RecyclerView.Adapter sensorAdapter;
     private RecyclerView.LayoutManager sensorLayoutManager;
-    public static SensorManager mySensorManager;
+    public static SensorManager sensorManager;
     public static List<Sensor> sensorList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mySensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensorList = mySensorManager.getSensorList(Sensor.TYPE_ALL);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
 
         sensorRecyclerView = findViewById(R.id.sensorRecyclerView);
 
@@ -31,6 +37,27 @@ public class MainActivity extends AppCompatActivity {
 
         sensorAdapter = new MyAdapter(sensorList);
         sensorRecyclerView.setAdapter(sensorAdapter);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int type = event.sensor.getType();
+        float[] values = event.values;
+        if (type == Sensor.TYPE_LIGHT && values[0] < 5) {
+            ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+            toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP,500);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }
